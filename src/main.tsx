fe6b175794480e58d59d936de4ac42fd38a86cf4
Mariza0 <maria.net@mail.ru>
2024-03-31 23:53:30 +0200

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './App.css';
 import './index.css';
 // import { ThemeProvider } from '@mui/material/styles';
 // import theme from './theme.tsx'; // Импортируйте созданную тему
