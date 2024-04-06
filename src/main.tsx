2dbb69303acae68086d04b632fbe3e04da9f171f
Mariza0 <maria.net@mail.ru>
2024-04-06 15:09:17 +0200

    change link

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './App.css';
 import './index.css';
-// import { ThemeProvider } from '@mui/material/styles';
-// import theme from './theme.tsx'; // Импортируйте созданную тему
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <ThemeProvider theme={theme}>
+
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  // </ThemeProvider>,
+  
 )
